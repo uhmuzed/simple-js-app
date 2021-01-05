66c5959182b0f2841112d3ea212e6a694217b337
uhmuzed <75945396+uhmuzed@users.noreply.github.com>
2021-01-04 22:06:43 -0600

    added pokemonList var

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+let pokemonList = [
+  {
+    name: "Obstagoon",
+    dexNumber: 862,
+    type: ["Dark", "Normal"],
+    category: "Blocking",
+    height: 5.03
+  },
+  {
+    name: "Zacian",
+    dexNumber: 888,
+    type: ["Fairy", "Steel"],
+    category: "Warrior",
+    height: 9.02
+  },
+  {
+    name: "Zamazenta",
+    dexNumber: 889,
+    type: ["Fighting", "Steel"],
+    category: "Warrior",
+    height: 9.06
+  }
+];
